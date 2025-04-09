23c4cf0ab4c802c1407600767991c91d82f8240f
minjikimkim2222 <olivia0929@naver.com>
2025-04-09 13:41:05 +0900

    fork 이후 커밋 정상작동?

--- be/src/main/java/com/podmate/domain/token/api/TokenRestController.java
+++ be/src/main/java/com/podmate/domain/token/api/TokenRestController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.podmate.domain.token.dto.TokenResponseDto.AuthenticationResponse;
 
 // Controller에는 오직 Service 로직 위임만을 해야 하는데, 비즈니스 로직이 보인다. 리팩토링 함..
 @RestController
